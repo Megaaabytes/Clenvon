cccff9e8c36125e7d8a9ee59e8d43f7afaab5e82
Megaaabytes <73676100+Megaaabytes@users.noreply.github.com>
2021-10-03 18:22:31 -0400

    Clenvon 1.0.2

---
+++ IO/GetFile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clenvon.Parsing;
+using System.IO;
+
+namespace Clenvon.IO
+{
+    public class GetFile
+    {
+        public static bool CheckFiles(string path)
+        {
+            if(File.Exists(path) == false)
+            {
+                Console.WriteLine("\n[FATAL ERROR] The specified file does not exist.");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static string GetFileFromData(string projectDirectory, string data)
+        {
+            string dataPath = Path.Combine(projectDirectory, data);
+            return dataPath;
+        }
+    }
+}
